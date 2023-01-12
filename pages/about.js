f0368bf641c1057dbd17c2e6807420ecdc8951bf
Omarhuba <omaruba@gmail.com>
2023-01-12 23:49:03 +0100

    some changes

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>about</div>
-  )
-}
+    <div>
+      <h1>This is About page</h1>
+    </div>
+  );
+};
 
-export default About
+export default About;
