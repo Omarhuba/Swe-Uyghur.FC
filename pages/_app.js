7708338e5f7ea0720e3571a60fa22a4f70038b1c
Omarhuba <omaruba@gmail.com>
2023-01-20 00:07:34 +0100

    google popup login is done, and login page ,home page is done, added video for home

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function App({ Component, pageProps }) {
-  return (
+  return (<>
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    <ToastContainer autoClose={false} />
+  </>
   );
 }
--- pages/auth/login.jsx
+++ pages/auth/login.jsx
@@ -1,40 +1,48 @@
-import React from 'react'
-import { FcGoogle } from 'react-icons/fc'
+import React from "react";
+import { FcGoogle } from "react-icons/fc";
 // import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
 // import { signInWithPopup ,GoogleAuthProvider } from "firebase/auth";
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { auth } from '../../utils/firebase'
-import { useRouter } from 'next/router';
-
+import { auth } from "../../utils/firebase";
+import { useRouter } from "next/router";
+import { toastSuccess, toastError } from "../../utils/toastify";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const Login = () => {
-      const router = useRouter()
 
-    // * Sign Google providfer
-    const googleProvider = new GoogleAuthProvider()
-    const GoogleLogin = async () => {
-        try {
-            const result = await signInWithPopup(auth, googleProvider);
-            router.push("/")
-          } catch (error) {
-            console.log(error);
-          }
-        };
+  // react-firebase-hooks
+  const [user, loading] = useAuthState(auth)
+  console.log(user)
+  const router = useRouter();
+  // const onClick = () => toast('Toast is good', { hideProgressBar: true, autoClose: 3000, type: 'success' })
 
+  // * Sign Google providfer
+  const googleProvider = new GoogleAuthProvider();
+  const GoogleLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider);
+      router.push("/dashboard");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <div className='shadow-xl mt-32 p-10 text-gray-700'>
-        <h1 className='text-2xl font-medium'>Join Today!</h1>
-        <div className='py-4'>
-            <h3 className='py-4'>Sign in with one of the providers</h3>
-            <button onClick={GoogleLogin} className='text-white bg-gray-700 w-full font-medium rounded-lg align-middle p-4 flex gap-2'>
-
-                <FcGoogle className='text-2xl' />
-                Sign in with Google
-            </button>
-        </div>
+    <div className="shadow-xl mt-32 p-10 text-gray-700">
+      <h1 className="text-2xl font-medium">Join Today!</h1>
+      <div className="py-4">
+        <h3 className="py-4">Sign in with one of the providers</h3>
+        <button
+          onClick={GoogleLogin}
+          className="text-white bg-gray-700 w-full font-medium rounded-lg align-middle p-4 flex gap-2"
+        >
+          <FcGoogle className="text-2xl" />
+          Sign in with Google
+        </button>
+      </div>
+      {/* <button onClick={onClick}> Click Me</button> */}
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
