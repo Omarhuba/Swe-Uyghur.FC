5af3bf7330b739be3a3a5c375c877f49d68e6b46
Omarhuba <omarhuba@gmail.com>
2023-07-05 00:21:23 +0200

    test

--- utils/firebase.jsx
+++ utils/firebase.jsx
@@ -8,7 +8,7 @@ import { getStorage } from "firebase/storage";
 // https://firebase.google.com/docs/web/setup#available-libraries
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
+//! annat sätt
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
